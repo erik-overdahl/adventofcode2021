eca0958464a9b4f3f400b2b7b0cdd0bc7b692db6
Erik Overdahl <erik.overdahl@wisetechglobal.com>
2021-12-01 14:30:08 -0500

    Get rid of isMonotonicallyIncreasing
    thought that would be relevant. wasn't.

--- golang/day01/day01_test.go
+++ golang/day01/day01_test.go
@@ -5,24 +5,7 @@ import (
 	"testing"
 )
 
-func TestIsMonotonicallyIncreasing(t *testing.T) {
-	type testcase struct {
-		input	 []int
-		expected bool
 	}
-	largerSlice := []int{1, 2, 3, 2, 1}
-	cases := []testcase{
-		{input: []int{1, 2, 3}, expected: true},
-		{input: []int{2, 2, 3}, expected: false},
-		{input: []int{3, 2, 3}, expected: false},
-		{input: largerSlice[:3], expected: true},
-		{input: largerSlice[1:5], expected: false},
-	}
-	for _, x := range cases {
-		actual := isMonotonicallyIncreasing(x.input)
-		if actual != x.expected {
-			t.Fatalf("Input %v: expected %v, got %v", x.input, x.expected, actual)
-		}
 	}
 }
 
