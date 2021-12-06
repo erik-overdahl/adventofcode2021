da0153810a03fe16fbb0233b8472ea258c7625b9
Erik Overdahl <erik.overdahl@gmail.com>
2021-12-06 15:30:35 -0500

    Run days in parallel

--- golang/main.go
+++ golang/main.go
@@ -5,12 +5,16 @@ import (
 	"aoc2021/day02"
 	"aoc2021/day03"
 	"aoc2021/day04"
+	"aoc2021/day05"
 	"aoc2021/day06"
 	"aoc2021/utils"
 	"path/filepath"
+	"sync"
 
 	"fmt"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	solutions := []utils.AOCDay{
@@ -18,17 +22,28 @@ func main() {
 		&day02.Solution{},
 		&day03.Solution{},
 		&day04.Solution{},
+		&day05.Solution{},
 		&day06.Solution{},
 	}
 	for _, s := range solutions {
-		day := s.Day()
-		filename := fmt.Sprintf("../inputs/0%02d.txt", day)
-		path, err := filepath.Abs(filename)
-		if err != nil {
-			panic(err)
-		}
-		s.Init(path)
-		fmt.Printf("Day %d Part 1 solution: %s\n", day, s.Part1())
-		fmt.Printf("Day %d Part 2 solution: %s\n", day, s.Part2())
+		wg.Add(1)
+		go func(solutions utils.AOCDay) {
+			defer wg.Done()
+			runDay(solutions)
+		}(s)
 	}
+	wg.Wait()
+}
+
+func runDay(s utils.AOCDay) {
+	day := s.Day()
+	filename := fmt.Sprintf("../inputs/0%02d.txt", day)
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		panic(err)
+	}
+	s.Init(path)
+	p1 := fmt.Sprintf("Day %d Part 1 solution: %s\n", day, s.Part1())
+	p2 := fmt.Sprintf("Day %d Part 2 solution: %s\n", day, s.Part2())
+	fmt.Printf("%s%s", p1, p2)
 }
