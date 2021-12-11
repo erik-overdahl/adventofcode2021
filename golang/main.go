e5123739ac53a3e88c5e7cc15b5d655282aad776
Erik Overdahl <erik.overdahl@gmail.com>
2021-12-11 13:41:38 -0500

    Print results in order

--- golang/main.go
+++ golang/main.go
@@ -21,36 +21,53 @@ import (
 	"fmt"
 )
 
+var solutions = []utils.AOCDay{
+	&day01.Solution{},
+	&day02.Solution{},
+	&day03.Solution{},
+	&day04.Solution{},
+	&day05.Solution{},
+	&day06.Solution{},
+	&day07.Solution{},
+	&day08.Solution{},
+	&day09.Solution{},
+	&day10.Solution{},
+	&day11.Solution{},
+}
+
 var wg sync.WaitGroup
 
 //go:embed inputs/*
 var inputs embed.FS
 
 func main() {
-	solutions := []utils.AOCDay{
-		&day01.Solution{},
-		&day02.Solution{},
-		&day03.Solution{},
-		&day04.Solution{},
-		&day05.Solution{},
-		&day06.Solution{},
-		&day07.Solution{},
-		&day08.Solution{},
-		&day09.Solution{},
-		&day10.Solution{},
-		&day11.Solution{},
-	}
+	outChan := make(chan output)
+	res := make(chan []string, len(solutions))
+	go func() {
+		outputs := make([]string, len(solutions))
+		for m := range outChan {
+			outputs[m.pos] = m.msg
+		}
+		res <- outputs
+	}()
 	for _, s := range solutions {
 		wg.Add(1)
 		go func(solutions utils.AOCDay) {
 			defer wg.Done()
-			runDay(solutions)
+			msg := runDay(solutions)
+			outChan <- output{solutions.Day() - 1, msg}
 		}(s)
 	}
 	wg.Wait()
+	close(outChan)
+	outputs := <-res
+	close(res)
+	for day := range outputs {
+		fmt.Println(outputs[day])
+	}
 }
 
-func runDay(s utils.AOCDay) {
+func runDay(s utils.AOCDay) string {
 	day := s.Day()
 	filename := fmt.Sprintf("inputs/0%02d.txt", day)
 	inputBytes, err := inputs.ReadFile(filename)
@@ -62,12 +79,17 @@ func runDay(s utils.AOCDay) {
 	start := time.Now()
 	p1 := s.Part1()
 	elapsed := time.Since(start)
-	s1 := fmt.Sprintf("Day %d Part 1 solution: %d  (took %s)\n", day, p1, elapsed)
+	s1 := fmt.Sprintf("Part 1: %-19d  (took %s)", p1, elapsed)
 
 	start = time.Now()
 	p2 := s.Part2()
 	elapsed = time.Since(start)
-	s2 := fmt.Sprintf("Day %d Part 2 solution: %d  (took %s)\n", day, p2, elapsed)
+	s2 := fmt.Sprintf("Part 2: %-19d  (took %s)", p2, elapsed)
 
-	fmt.Printf("%s%s", s1, s2)
+	return fmt.Sprintf("Day %d\n%s\n%s\n", day, s1, s2)
+}
+
+type output struct {
+	pos int
+	msg string
 }
