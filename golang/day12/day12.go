c2e1cd492cf60fc76fb82b9003fbcc01d6081918
Erik Overdahl <erik.overdahl@gmail.com>
2021-12-12 20:57:37 -0500

    Significantly improve Day 12
    Represent the nodes as integers, keep track of seen by toggling bits in
    a single integer, and use int slices wherever possible rather than maps
    
    original (with memoization!):
    Part 1: 3497                 (took 49.700411ms)
    Part 2: 93686                (took 115.154647ms)
    
    improved (no memoization):
    Part 1: 3497                 (took 1.580236ms)
    Part 2: 93686                (took 32.575946ms)
    
    improved (memoized):
    Part 1: 3497                 (took 706.797µs)
    Part 2: 93686                (took 4.766899ms)
    
    These are not exhaustive benchmarks, just timings. Looks pretty
    promising though. And I think the readability hit to the code is
    actually not that bad.

--- golang/day12/day12.go
+++ golang/day12/day12.go
@@ -5,78 +5,89 @@ import (
 	"strings"
 )
 
+func part1(input []string) int {
+	graph := makeGraph(input)
+	memoizer := make(map[int]int)
+	result := countPaths(graph, 0, 1<<len(graph.positions), false, memoizer)
+	return result
+}
+
+func part2(input []string) int {
+	graph := makeGraph(input)
+	memoizer := make(map[int]int)
+	result := countPaths(graph, 0, 1<<len(graph.positions), true, memoizer)
+	return result
+}
+
 type graph struct {
-	adjList map[string][]string
+	positions []string
+	edges     [][]int
 }
 
-var memoizer = make(map[string]int)
-
-func MakeGraph(input []string) *graph {
-	adjacencyList := make(map[string][]string)
-	for _, line := range input {
-		nodes := strings.Split(line, "-")
-		if adjacent, exists := adjacencyList[nodes[0]]; exists {
-			adjacencyList[nodes[0]] = append(adjacent, nodes[1])
-		} else {
-			adjacencyList[nodes[0]] = []string{nodes[1]}
-		}
-		if adjacent, exists := adjacencyList[nodes[1]]; exists {
-			adjacencyList[nodes[1]] = append(adjacent, nodes[0])
-		} else {
-			adjacencyList[nodes[1]] = []string{nodes[0]}
-		}
+func countPaths(g *graph, v, seen int, canReturn bool, memoizer map[int]int) int {
+	if v == 1 { // end
+		return 1
 	}
-	adjacencyList["end"] = []string{}
-	return &graph{
-		adjList: adjacencyList,
+	memKey := (seen << 9) + (v << 1)
+	if canReturn {
+		memKey |= 1
 	}
-}
-
-func (g *graph) countPaths(v string, seen map[string]bool, canReturn bool) int {
-	visited := seen[v]
-	memKey := fmt.Sprintf("%s_%v_%v", v, seen, canReturn)
 	if result, exists := memoizer[memKey]; exists {
 		return result
 	}
-	if v == "end" {
-		return 1
-	} else if visited {
-		if v == "start" {
+	visited := bitAt(seen, v) == 1
+	if visited {
+		if v == 0 { // start
 			return 0
-		} else if byte(v[0]) > 90 && !canReturn {
+		}
+		smallCave := byte(g.positions[v][0]) > 90
+		if smallCave && !canReturn {
 			return 0
-		} else if byte(v[0]) > 90 && canReturn {
+		} else if smallCave && canReturn {
 			canReturn = false
 		}
 	}
-	// fucking fine
-	seenCopy := make(map[string]bool)
-	for k, v := range seen {
-		seenCopy[k] = v
-	}
-	seenCopy[v] = true
+	seen = setBit(seen, v)
 	sum := 0
-	for _, n := range g.adjList[v] {
-		sum += g.countPaths(n, seenCopy, canReturn)
+	for _, n := range g.edges[v] {
+		sum += countPaths(g, n, seen, canReturn, memoizer)
 	}
 	memoizer[memKey] = sum
 	return sum
 }
 
-func part1(input []string) int {
-	graph := MakeGraph(input)
-	seen := make(map[string]bool)
-	for v := range graph.adjList {
-		seen[v] = false
+func makeGraph(input []string) *graph {
+	positions := map[string]int{"start": 0, "end": 1}
+	edges := [][]int{{}, {}}
+	for _, line := range input {
+		nodes := strings.Split(line, "-")
+		pos1, exists := positions[nodes[0]]
+		if !exists {
+			pos1 = len(edges)
+			positions[nodes[0]] = pos1
+			edges = append(edges, []int{})
+		}
+		pos2, exists := positions[nodes[1]]
+		if !exists {
+			pos2 = len(edges)
+			positions[nodes[1]] = pos2
+			edges = append(edges, []int{})
+		}
+		edges[pos1] = append(edges[pos1], pos2)
+		edges[pos2] = append(edges[pos2], pos1)
 	}
-	return graph.countPaths("start", seen, false)
+	fmt.Println(positions)
+	gPositions := make([]string, len(positions))
+	for name, pos := range positions {
+		gPositions[pos] = name
+	}
+	return &graph{positions: gPositions, edges: edges}
 }
 
-func part2(input []string) int {
-	graph := MakeGraph(input)
-	seen := make(map[string]bool)
-	for v := range graph.adjList {
-		seen[v] = false
-	}
-	return graph.countPaths("start", seen, true)
+func bitAt(n int, pos int) int {
+	return (n >> pos) & 1
+}
+
+func setBit(n int, pos int) int {
+	return n | 1<<pos
 }
